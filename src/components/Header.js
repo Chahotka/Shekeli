04212f599bf4e2f1442f2a2bde5ffb3f9baf242d
Arslan Rasulov <magomrak@yandex.ru>
2023-06-20 17:29:23 +0200

    Header cmp

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/header.scss'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container container">
+        <div className="header__title">Shekeli</div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
