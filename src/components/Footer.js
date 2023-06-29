d5aa68a5436e8bb58d55f2e783157f1c53ec1d51
Arslan Rasulov <magomrak@yandex.ru>
2023-06-29 15:14:41 +0200

    Footer cmp

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/footer.scss'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Made by Chahotka</span>
+    </footer>
+  )
+}
+
+export default Footer
